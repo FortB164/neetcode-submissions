50ed54c02aed383ac64bacd9f2841f98bbf5e9b3
Tanmay Malik <94996894+FortB164@users.noreply.github.com>
2026-07-17 19:47:29 +0100

    Update submission-5.java

--- Data Structures & Algorithms/is-anagram/submission-5.java
+++ Data Structures & Algorithms/is-anagram/submission-5.java
@@ -6,9 +6,7 @@ class Solution {
         char [] t1 = t.toCharArray();
         Arrays.sort(s1);
         Arrays.sort(t1);
-        String s2 = new String(s1);
-        String t2 = new String(t1);
 
-        return s2.equals(t2);
+        return Arrays.equals(t1,s1);
     }
 }
