34d4fb48f03a9ef47a8f3f2f97ad14834d366637
Tanmay Malik <94996894+FortB164@users.noreply.github.com>
2026-07-22 17:23:45 +0100

    Update submission-1.java

--- Data Structures & Algorithms/anagram-groups/submission-1.java
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -3,14 +3,14 @@ class Solution {
         
         Map<String, List<String>> map = new HashMap<>();
         for(String s : strs){
-            map.putIfAbsent(getSortedKey(s), new ArrayList<>());
-            map.get(getSortedKey(s)).add(s);
+            map.putIfAbsent(getSortedKey(s), new ArrayList<>()); // create a bucket by putting down sorted string as key and arraylist as value
+            map.get(getSortedKey(s)).add(s); // gets the sorted key of string s, which gets the value aka the arraylist and then just add string s to that arraylist.
         }
-        return new ArrayList<>(map.values());
+        return new ArrayList<>(map.values()); // return the buckets
     }
 
-    public static String getSortedKey(String x){
-        char [] str = x.toCharArray();
+    public static String getSortedKey(String x){  // returns the string sorted alphabetically that becomes the hashmap key
+        char [] str = x.toCharArray();            // cat becomes act, tree becomes eert
         Arrays.sort(str);
         String y = new String(str);
         return y;
